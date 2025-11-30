8a2a92d0cd40f3ed7af194e2e18f8ec2b2646efa
Mauricio <mauriciorojasanchez@gmail.com>
2025-11-30 17:46:22 -0500

    feat: added history context per chat.

--- main.py
+++ main.py
@@ -25,7 +25,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-client = genai.Client(api_key=os.getenv("GEMINI_APIKEY"))
+SYSTEM_PROMPT_TEMPLATE = """
+Eres un experto socio en finanzas y toma de decisiones.
+Tu rol es ayudar al gerente {username} del hotel.
+Datos del hotel:
+- Ingresos semanales: {income} soles
+- Gastos semanales: {expenses} soles
+
+Tu estilo:
+- Sé amable, honesto y profesional.
+- No inventes información que no tengas.
+- Explica de forma clara y en términos simples.
+- Responde SIEMPRE en español.
+- Siempre menciona los ingresos y gastos de la siguiente manera: Con la informacion que tengo a la mano de tu hotel.
+
+Ahora empieza a ayudar al gerente con sus dudas o necesidades.
+"""
+
+
+genai_client = genai.Client(api_key=os.getenv("GEMINI_APIKEY"))
+memory = {}
 
 @app.get("/")
 def root():
@@ -33,33 +52,46 @@ def root():
 
 @app.get("/models") # Get the list of models that I can only use
 def models():
-    models = client.models.list()
+    models = genai_client.models.list()
     return {"message": models }
 
 @app.post("/chat")
 async def chat(req: ChatRequest):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents= [
-            {
-                "role": "user",
+    
+    # 1. Only build the system context on a NEW Conversation
+    if req.conversation_id not in memory:
+        system_prompt = SYSTEM_PROMPT_TEMPLATE.format(username=req.username, income=req.income, expenses=req.expenses)
+        
+        memory[req.conversation_id] = [
+            { 
+                "role": "user", 
                 "parts": [
                     {
-                        "text": f"You are an expert partner in finance and decision making. The manager you are going to help is called {req.username} and the hotel has {req.income} soles of weekly income and {req.expenses} soles of weekly expenses. Be kind, honest and professional in your responses. Always respond in Spanish."
-                    }
-                ]
-            },
-            {
-                "role": "user",
-                "parts": [
-                    {
-                        "text": req.message
+                        "text": system_prompt
                     }
                 ]
             }
         ]
+    
+    # 2. Append the user message into conversation's history context
+    memory[req.conversation_id].append({
+        "role": "user",
+        "parts": [{"text": req.message}]
+    })
+    
+    # 3. Call Gemini model
+    response = genai_client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents= memory[req.conversation_id]
     )
 
+    # 4. Store assistan message into conversation's history context
+    memory[req.conversation_id].append({
+        "role": "model",
+        "parts": [{"text": response.text}]
+    })
+    
+    # 5. Returns model's response
     return {
         "message": response.text
     }
