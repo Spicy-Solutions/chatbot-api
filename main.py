5387eead36551c9bb766e2f737e61a57a21d6123
Mauricio <mauriciorojasanchez@gmail.com>
2025-11-30 19:52:13 -0500

    chore: added permissions to web.

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
-        "https://sweet-manager-web-application.vercel.app"
+        "https://sm-front-end-nine.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
